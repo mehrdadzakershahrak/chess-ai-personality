8ccfc8c15bc080799d63227525dd0468c0ca19ec
mehrdadzakershahrak <zaker.mehrdad@gmail.com>
2023-12-01 10:47:16 -0600

    working on mpersonality traits

--- data_generation/chess_environment.py
+++ data_generation/chess_environment.py
@@ -1,9 +1,9 @@
 
 import chess
-
 class ChessGame:
-    def __init__(self):
+    def __init__(self, trait):
         self.board = chess.Board()
+        self.trait = trait
 
     def make_move(self, move):
         # Ensure the move is legal
@@ -28,10 +28,35 @@ class ChessGame:
         else:
             return "Game in progress"
 
+    def calculate_best_aggressive_move(self, board):
+        # Implement the logic to calculate the best aggressive move
+        pass
+
+    def calculate_best_skeptical_move(self, board):
+        # Implement the logic to calculate the best skeptical move
+        pass
+
+    def calculate_best_cautious_move(self, board):
+        # Implement the logic to calculate the best cautious move
+        pass
+
+    def calculate_best_innovative_move(self, board):
+        # Implement the logic to calculate the best innovative move
+        pass
+
     def simulate_game(self, player1, player2):
         while True:
             if self.board.turn:
-                move = player1.get_move(self.board)
+                if self.trait == "Aggressive":
+                    move = self.calculate_best_aggressive_move(self.board)
+                elif self.trait == "Skeptical":
+                    move = self.calculate_best_skeptical_move(self.board)
+                elif self.trait == "Cautious":
+                    move = self.calculate_best_cautious_move(self.board)
+                elif self.trait == "Innovative":
+                    move = self.calculate_best_innovative_move(self.board)
+                else:
+                    move = player1.get_move(self.board)
             else:
                 move = player2.get_move(self.board)
 
@@ -41,3 +66,19 @@ class ChessGame:
             status = self.game_status()
             if status != "Game in progress":
                 return status
+
+    def calculate_best_aggressive_move(self, board):
+        # Implement the logic to calculate the best aggressive move
+        pass
+
+    def calculate_best_skeptical_move(self, board):
+        # Implement the logic to calculate the best skeptical move
+        pass
+
+    def calculate_best_cautious_move(self, board):
+        # Implement the logic to calculate the best cautious move
+        pass
+
+    def calculate_best_innovative_move(self, board):
+        # Implement the logic to calculate the best innovative move
+        pass
