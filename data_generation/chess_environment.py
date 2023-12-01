bc4ee24ebdf7a902b4f38c0c7444571d291cf6fd
mehrdadzakershahrak <zaker.mehrdad@gmail.com>
2023-12-01 13:37:45 -0600

    working on attention architecture #3

--- data_generation/chess_environment.py
+++ data_generation/chess_environment.py
@@ -1,14 +1,18 @@
 
 import chess
 from trait_mapping import TraitMapping
+from chess_transformer import ChessTransformer, TraitEncoder, ExplainableChessTransformer
 
 class ChessGame:
-    def __init__(self, trait):
+    def __init__(self, player1_trait, player2_trait):
         self.board = chess.Board()
-        self.trait = trait
+        self.trait_encoder = TraitEncoder()
+        self.chess_transformer = ChessTransformer(self.trait_encoder)
+        self.explainable_transformer = ExplainableChessTransformer(self.chess_transformer)
+        self.player1_trait = player1_trait
+        self.player2_trait = player2_trait
 
     def make_move(self, move):
-        # Ensure the move is legal
         if move in self.board.legal_moves:
             self.board.push(move)
             return True
@@ -30,149 +34,22 @@ class ChessGame:
         else:
             return "Game in progress"
 
-    def calculate_best_aggressive_move(self, board):
-        # 1. List all legal moves
-        # 2. Prioritize moves that:
-        #    - Capture opponent's pieces, especially near the opponent's king
-        #    - Put the opponent's king in check
-        #    - Open lines for attacking the opponent's king
-        # 3. Select the move that maximizes positional advantage or material gain
-        #    (This may require an evaluation function)        
-        legal_moves = list(board.legal_moves)
-        best_move = None
-        best_score = float('-inf')
+    def get_best_move(self, trait):
+        # Here, we use the transformer model to decide the best move
+        board_fen = self.board.fen()
+        move = self.explainable_transformer(board_fen, trait)
+        return move
 
-        for move in legal_moves:
-            # Make a copy of the board to simulate the move
-            temp_board = board.copy()
-            temp_board.push(move)
-
-            # Evaluate the move based on capturing opponent's pieces, checking the opponent's king, and opening lines for attacking the opponent's king
-            score = self.evaluate_move(temp_board, self.trait)
-
-            # Update the best move if the current move has a higher score
-            if score > best_score:
-                best_move = move
-                best_score = score
-
-        return best_move
-
-    def evaluate_move(self, board, trait):
-        # Define weights for different traits
-        capture_weight, check_weight, open_lines_weight = self.get_trait_weights(trait)
-
-        score = 0
-
-        # Capture opponent's pieces
-        captured_piece = board.piece_at(board.peek())
-        if captured_piece is not None:
-            score += capture_weight * captured_piece.piece_type
-
-        # Check the opponent's king
-        if board.is_check():
-            score += check_weight
-
-        # Open lines for attacking the opponent's king
-        for square in board.attackers(not board.turn, board.king(not board.turn)):
-            score += open_lines_weight
-
-        return score
-    
-    def get_trait_weights(self, trait):
-        if trait == "Aggressive":
-            return 3, 2, 1  # Prioritize captures and checks
-        elif trait == "Skeptical":
-            return 1, 1, 1  # Balanced approach
-        elif trait == "Cautious":
-            return 1, 2, 3  # Prioritize open lines and safety
-        elif trait == "Innovative":
-            return 2, 3, 1  # Prioritize unusual moves and checks
-        else:
-            return 1, 1, 1  # Default weights
-    
-    def calculate_best_skeptical_move(self, board):
-        # 1. List all legal moves
-        # 2. Prioritize moves that:
-        #    - Reinforce your own position (e.g., developing pieces, controlling the center)
-        #    - Avoid unnecessary risks (e.g., premature attacks, uncalculated sacrifices)
-        #    - Focus on solidifying your position rather than attacking
-        #  use evaluate_move(board) to evaluate the move
-        legal_moves = list(board.legal_moves)
-        best_move = None
-        best_score = float('-inf')
-
-        for move in legal_moves:
-            # Make a copy of the board to simulate the move
-            temp_board = board.copy()
-            temp_board.push(move)
-
-            # Evaluate the move based on reinforcing own position, avoiding unnecessary risks, and focusing on solidifying position
-            score = self.evaluate_move(temp_board, self.trait)
-
-            # Update the best move if the current move has a higher score
-            if score > best_score:
-                best_move = move
-                best_score = score
-
-        return best_move        
-
-    def calculate_best_cautious_move(self, board):
-        legal_moves = list(board.legal_moves)
-        best_move = None
-        best_score = float('-inf')
-
-        for move in legal_moves:
-            # Make a copy of the board to simulate the move
-            temp_board = board.copy()
-            temp_board.push(move)
-
-            # Evaluate the move based on reinforcing own position, avoiding unnecessary risks, and focusing on solidifying position
-            score = self.evaluate_move(temp_board, self.trait)
-
-            # Update the best move if the current move has a higher score
-            if score > best_score:
-                best_move = move
-                best_score = score
-
-        return best_move
-        
-    def calculate_best_innovative_move(self, board):
-        legal_moves = list(board.legal_moves)
-        best_move = None
-        best_score = float('-inf')
-
-        for move in legal_moves:
-            # Make a copy of the board to simulate the move
-            temp_board = board.copy()
-            temp_board.push(move)
-
-            # Evaluate the move based on potential benefits and risks
-            score = self.evaluate_move(temp_board, self.trait)
-
-            # Update the best move if the current move has a higher score
-            if score > best_score:
-                best_move = move
-                best_score = score
-
-        return best_move
-        
-    def simulate_game(self, player1_trait, player2_trait):
-        trait_mapping_player1 = TraitMapping(player1_trait)
-        trait_mapping_player2 = TraitMapping(player2_trait)
-
+    def simulate_game(self):
         while True:
             if self.board.turn:
-                # It's player1's turn; use player1's trait strategy
-                trait_strategy = trait_mapping_player1.get_trait_strategy()
-                move = trait_strategy(self.board)
+                move = self.get_best_move(self.player1_trait)
             else:
-                # It's player2's turn; use player2's trait strategy
-                trait_strategy = trait_mapping_player2.get_trait_strategy()
-                move = trait_strategy(self.board)
+                move = self.get_best_move(self.player2_trait)
 
             if not self.make_move(move):
                 return "Invalid move"
-            
+
             status = self.game_status()
             if status != "Game in progress":
                 return status
